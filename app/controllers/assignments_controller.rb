c8fea64f90a383b945760bc08f3425a3ce3388c5
Stephen Lagree <slagree@dyn-72-33-209-194.uwnet.wisc.edu>
2014-03-25 17:51:50 -0500

    added action for adding assignments

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -2,4 +2,16 @@ class AssignmentsController < ApplicationController
 	def index
 		@assignments = Assignment.all
 	end
+
+	def assignTasks
+		assigner=params[:assigner]
+		assignee=params[:assignee]
+		locationIds=params[:locationIds]
+		locationIds.each do |id|
+			Assignment.create(UserIdAssigner: assigner,user_id: assignee,location_id: id,Status: 1)
+		end
+
+		render :json => {success: 'true', redirect: 'http://localhost:3000/' }
+		#redirect_to :controller=>'dashboard', :action=> 'index'
+	end
 end
