34b8642056ebdbd2dbb61bf22783373d94bc8080
Stephen Lagree <stephen.lagreee@gmail.com>
2014-03-30 15:22:46 -0500

    Clear button on map now clears drawn polygons

--- public/arcgismap.js
+++ public/arcgismap.js
@@ -4,13 +4,13 @@ var map, dialog;
         "esri/map", "esri/toolbars/draw", "esri/layers/FeatureLayer",
         "esri/symbols/SimpleFillSymbol", "esri/symbols/SimpleLineSymbol","esri/arcgis/utils", 
         "esri/renderers/SimpleRenderer", "esri/symbols/PictureFillSymbol","esri/graphic", "esri/lang","esri/tasks/query",
-        "dojo/_base/Color", "dojo/number", "dojo/dom-style", 
+        "dojo/_base/Color", "dojo/number", "dojo/dom-style", "esri/InfoTemplate",
         "dijit/TooltipDialog","dojo/dom","dojo/on", "dijit/popup", "dojo/domReady!"
       ], function(
         Map, Draw, FeatureLayer,
         SimpleFillSymbol, SimpleLineSymbol, arcgisUtils,
         SimpleRenderer,PictureFillSymbol, Graphic, esriLang,Query,
-        Color, number, domStyle, 
+        Color, number, domStyle, InfoTemplate,
         TooltipDialog, dom,on,dijitPopup
       ) {
         
@@ -42,6 +42,7 @@ var map, dialog;
     
         map.infoWindow.resize(245,125);
         
+
         dialog = new TooltipDialog({
           id: "tooltipDialog",
           style: "position: absolute; width: 250px; font: normal normal normal 10pt Helvetica;z-index:100"
@@ -88,37 +89,16 @@ var map, dialog;
 
         
           map.on("update-end",function(){
-          //console.log('update-end')
-          map.graphics.clear();
-          var currentSurveySites = map.getLayer(map.graphicsLayerIds[0]);
-          //console.log(currentSurveySites.visible)
-
-          for(var i=0;i<currentSurveySites.graphics.length;i++){
-              if (assigned[currentSurveySites.graphics[i].attributes.GTPOLYS_]==1){
-                        var highlightGraphic = new Graphic(currentSurveySites.graphics[i].geometry,assignedSymbol);
-              }else if (assigned[currentSurveySites.graphics[i].attributes.GTPOLYS_]==2){
-                        var highlightGraphic = new Graphic(currentSurveySites.graphics[i].geometry,completedSymbol);
-              }else if (assigned[currentSurveySites.graphics[i].attributes.GTPOLYS_]==3){
-                        var highlightGraphic = new Graphic(currentSurveySites.graphics[i].geometry,revisitSymbol);
-              }else {
-                        var highlightGraphic = new Graphic(currentSurveySites.graphics[i].geometry,notAssignedSymbol);
-              }
-              if (currentSurveySites.visible==true){
-                map.graphics.add(highlightGraphic);
-              }
-            }
+              clear()
           });
+
           map.on("zoom-end",function(evt){
 
-            //if (evt.zoomFactor<1){
-            //  console.log(evt.zoomFactor)
-            //  console.log(map.getScale())
             if ( (evt.zoomFactor<1) && (map.getScale()>1000000) ){
                 map.graphics.clear();
                 surveySites.visible=false;
-            }else{
+            }else if(map.getScale()<1000000){
                 surveySites.visible=true;
-
             }
           });
 
@@ -130,24 +110,6 @@ var map, dialog;
           
         });
                 
-        //listen for when the onMouseOver event fires on the countiesGraphicsLayer
-        //when fired, create a new graphic with the geometry from the event.graphic and add it to the maps graphics layer
-        // surveySites.on("mouse-over", function(evt){
-        //   var t = "<b>${NAME}</b><hr>Description"
-  
-        //   var content = esriLang.substitute(evt.graphic.attributes,t);
-        //   var highlightGraphic = new Graphic(evt.graphic.geometry,highlightSymbol);
-        //   map.graphics.add(highlightGraphic);
-          
-        //   dialog.setContent(content);
-
-        //   domStyle.set(dialog.domNode, "opacity", 0.85);
-        //   dijitPopup.open({
-        //     popup: dialog, 
-        //     x: evt.pageX,
-        //     y: evt.pageY
-        //   });
-        // });
     
         function initToolbar() {
           initializeFieldStaff();
@@ -164,6 +126,7 @@ var map, dialog;
                 assignTasks();
                 return;
             }else if(evt.target.id === "clear"){
+                clear()
                 return
             }
             var tool = evt.target.id.toLowerCase();
@@ -211,6 +174,31 @@ var map, dialog;
           });
           
         }
+
+        //This clears all graphics and polygons on map and redraws
+        function clear(){ 
+            map.graphics.clear();
+            tasks=[]
+            
+            var currentSurveySites = map.getLayer(map.graphicsLayerIds[0]);
+
+            for(var i=0;i<currentSurveySites.graphics.length;i++){
+              if (assigned[currentSurveySites.graphics[i].attributes.GTPOLYS_]==1){
+                        var highlightGraphic = new Graphic(currentSurveySites.graphics[i].geometry,assignedSymbol);
+              }else if (assigned[currentSurveySites.graphics[i].attributes.GTPOLYS_]==2){
+                        var highlightGraphic = new Graphic(currentSurveySites.graphics[i].geometry,completedSymbol);
+              }else if (assigned[currentSurveySites.graphics[i].attributes.GTPOLYS_]==3){
+                        var highlightGraphic = new Graphic(currentSurveySites.graphics[i].geometry,revisitSymbol);
+              }else {
+                        var highlightGraphic = new Graphic(currentSurveySites.graphics[i].geometry,notAssignedSymbol);
+              }
+              if (currentSurveySites.visible==true){
+                map.graphics.add(highlightGraphic);
+              }
+            }
+
+        }
+
         function assignTasks(){
             //get user to assign to
             var fieldStaffSelect = document.getElementById('FieldStaffSelect');
